058ccc888537c30bd6d5ecd7d26b415b0f207241
Charles Tex <texier.charles@hotmail.fr>
2016-01-10 16:31:30 +0100

    addExercice DONE

--- war/js/addTrainingPlan.js
+++ war/js/addTrainingPlan.js
@@ -1,4 +1,37 @@
 $(document).ready(function(){
+	var exercices = new Array();
+	var i = 0;
+	 $("#addexButton").prop('disabled',true);
+	    $("#inputTitle").keyup(function(){
+	        $("#addexButton").prop('disabled', this.value == "" ? true : false);     
+	    })
+	 $("#addexButton").click(function(){
+			
+		 	mytable = $('<table></table>').attr({ id: "basicTable" });
+			var exerciceTitle = $("#titleDescription").val();
+			var exerciceDescription = $("#exerciceDescription").val();
+			var time1 = $("#time1").val();
+			var time2 = $("#time2").val();
+			var time3 = $("#time3").val();
+			var time = time1+"h"+time2+"m"+time3+"s";
+			
+			
+			var row = $('<tr></tr>').attr({ class: ["class1", "class2", "class3"].join(' ') }).appendTo(mytable);
+			$('<td></td>').text(exerciceTitle).appendTo(row); 
+			$('<td></td>').text(exerciceDescription).appendTo(row);
+			$('<td></td>').text(time1+"h"+time2+"m"+time3+"s").appendTo(row); 
+			
+			var position = i+1;
+
+			mytable.appendTo("#box");
+			var myexercice = {"title":exerciceTitle, "description":exerciceDescription, "duree":time, "position":position };
+			
+			exercices[i] = myexercice;
+			console.log(exercices);
+			i++;
+			
+	 });
+	    
   $("#addTrainingPlan").click(function(){
 	  	$.post("addTrainingPlan",
     		  {
@@ -10,5 +43,15 @@ $(document).ready(function(){
     		  },
     		  function(data,status){
     		  });
-  		});
+	  	$.post("addExercice", 
+	  			{
+	  		cmd:"addExercice",
+		    title:$("#inputTitle").val(),
+		    exercices:JSON.stringify(exercices),
+		    //exercice:{exerciceTitle:$("titleDescription").val(), exerciceDescription:$("exerciceDescription").val()}, // need to be create before and out of the POST
+		  },
+		  function(data,status){
+		  }, "json");
+	
+  		}); 
 });
